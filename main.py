1163d601a1c689d781725104072329a50c7a9bc3
piotr <p.sztefko@gmail.com>
2022-04-09 18:57:16 +0200

    implemented tournament selection and fixed population generator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import sys
 
 
 def load_data(path):
@@ -11,7 +12,8 @@ def load_data(path):
 
     return list_of_rows
 
-def create_matrix(list_of_rows):
+
+def create_distances_matrix(list_of_rows):
     matrix = [[None] * SIZE for _ in range(SIZE)]
     for i in range(SIZE):
         for j in range(SIZE):
@@ -27,29 +29,48 @@ def generate_population_array():
     population = []
     for i in range(SIZE):
         array.append(i)
-    for i in range(SIZE):
+    for i in range(POPULATION):
         random.shuffle(array)
         population.append(array[:])
     return population
 
-def count_population_score(matrix, population):
+
+def count_population_scores(matrix, population):
     scores_array = []
-    for i in range(SIZE):
+    for i in range(POPULATION):
         sum = 0
         for j in range(SIZE):
-            index = population[i][j]
-            sum += matrix[i][index]
+            index = population[i % 5][j]
+            sum += matrix[i % 5][index]
         scores_array.append(sum)
     return scores_array
+
+
+def selection(population, scores, selection_pressure=3):
+
+    best_individual = []
+    best_score = sys.maxsize
+
+    random_indexes = random.sample(range(0, POPULATION), selection_pressure)
+
+    for index in random_indexes:
+
+        if scores[index] < best_score:
+            best_individual = population[index]
+
+    return best_individual
 
 
 list_of_rows = load_data('test.txt')
 
 SIZE = int(list_of_rows[0][0])
 
-matrix = create_matrix(list_of_rows)
+POPULATION = 200
+
+distance_matrix = create_distances_matrix(list_of_rows)
 
 population = generate_population_array()
 
-count_population_score(matrix, population)
+scores = count_population_scores(distance_matrix, population)
 
+selection(population, scores)
