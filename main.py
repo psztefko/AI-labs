a6849c7a076c13c1f902c2e0f87984a9007eab5f
piotr <p.sztefko@gmail.com>
2022-04-11 00:09:00 +0200

    implemented mutation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import random
 import sys
+from typing import List
 
 
 def load_data(path):
@@ -14,9 +15,9 @@ def load_data(path):
 
 
 def create_distances_matrix(list_of_rows):
-    matrix = [[None] * SIZE for _ in range(SIZE)]
-    for i in range(SIZE):
-        for j in range(SIZE):
+    matrix = [[None] * INDIVIDUAL_SIZE for _ in range(INDIVIDUAL_SIZE)]
+    for i in range(INDIVIDUAL_SIZE):
+        for j in range(INDIVIDUAL_SIZE):
             if j <= i:
                 matrix[i][j] = int(list_of_rows[i + 1][j])
             else:
@@ -27,30 +28,30 @@ def create_distances_matrix(list_of_rows):
 def generate_population_array():
     array = []
     population = []
-    for i in range(SIZE):
+    for i in range(INDIVIDUAL_SIZE):
         array.append(i)
-    for i in range(POPULATION):
+    for i in range(POPULATION_SIZE):
         random.shuffle(array)
         population.append(array[:])
     return population
 
 
-def count_population_scores(matrix, population):
+def count_population_scores(matrix, population) -> List:
     scores_array = []
-    for i in range(POPULATION):
+    for i in range(POPULATION_SIZE):
         sum = 0
-        for j in range(SIZE):
+        for j in range(INDIVIDUAL_SIZE):
             index = population[i % 5][j]
             sum += matrix[i % 5][index]
         scores_array.append(sum)
     return scores_array
 
 
-def tournament_selection(population, scores, selection_pressure=3):
+def tournament_selection(population, scores, selection_pressure=3) -> List:
     best_individual = []
     best_score = sys.maxsize
 
-    random_indexes = random.sample(range(0, POPULATION), selection_pressure)
+    random_indexes = random.sample(range(0, POPULATION_SIZE), selection_pressure)
 
     for index in random_indexes:
         if scores[index] < best_score:
@@ -63,10 +64,9 @@ def crossover(individual1, individual2):
 
     p1 = individual1
     p2 = individual2
-    print(p1)
-    print(p2)
-    cxpoint1 = random.randint(0, SIZE)
-    cxpoint2 = random.randint(0, SIZE - 1)
+
+    cxpoint1 = random.randint(0, INDIVIDUAL_SIZE)
+    cxpoint2 = random.randint(0, INDIVIDUAL_SIZE - 1)
     if cxpoint2 >= cxpoint1:
         cxpoint2 += 1
     else:  # Swap the two cx points
@@ -87,11 +87,29 @@ def crossover(individual1, individual2):
     return individual1, individual2
 
 
+def perform_mutation(individual, mutation_rate=0.3):
+    if random.random() < MUTATION_RATE:
+        if INDIVIDUAL_SIZE % 2 != 0:
+            subversion_amount = random.randint(2, (INDIVIDUAL_SIZE - 1)/2)
+        else:
+            subversion_amount = random.randint(2, INDIVIDUAL_SIZE/ 2)
+
+        if subversion_amount % 2 != 0:
+            subversion_amount -= 1
+
+        subversion_indexes = random.sample(range(0, INDIVIDUAL_SIZE), subversion_amount)
+
+        for i in range(0, subversion_amount - 1, 2):
+            individual[subversion_indexes[i]], individual[subversion_indexes[i + 1]] = individual[subversion_indexes[i + 1]], individual[subversion_indexes[i]]
+
+    return individual
+
 list_of_rows = load_data('test.txt')
 
-SIZE = int(list_of_rows[0][0])
-
-POPULATION = 200
+INDIVIDUAL_SIZE = int(list_of_rows[0][0])
+POPULATION_SIZE = 200
+MUTATION_RATE = 0.3
+SELECTION_PRESSURE = 3
 
 distance_matrix = create_distances_matrix(list_of_rows)
 
@@ -99,6 +117,13 @@ population = generate_population_array()
 
 scores = count_population_scores(distance_matrix, population)
 
-tournament_winners = [tournament_selection(population, scores) for _ in range(POPULATION)]
+tournament_winners = [tournament_selection(population, scores) for _ in range(POPULATION_SIZE)]
 
-crossover(tournament_selection(population, scores), tournament_selection(population, scores))
+population_after_crossover = []
+for i in range(0, len(population) - 1, 2):
+    temp_tuple = (crossover(tournament_winners[i], tournament_winners[i + 1]))
+    population_after_crossover.append(temp_tuple[0])
+    population_after_crossover.append(temp_tuple[1])
+
+print(population_after_crossover[0])
+print(perform_mutation(population_after_crossover[0]))
